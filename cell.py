0bc5084791e8edfaa5d5a22d12053e622deb2803
Sophie Walden <seamus.walden@maine.edu>
2025-03-24 19:35:38 -0400

    initial commit

---
+++ cell.py
@@ -0,0 +1,43 @@
+# Cell Class
+# Stores the attack/defense modifiers and the color
+# for each terrain type. Terrain types are defined in
+# cell_terrain.py
+#
+# Currently, Open terrain gives a +2 for an attacker and
+# nothing to a defender. Forest gives a +2 to the defender
+# and nothing to the attacker.
+#
+# Feel free to modify.
+
+
+import cell_terrain
+
+COLOR_DICT = {
+    cell_terrain.Terrain.Open: "olivedrab2",
+    cell_terrain.Terrain.Forest: "springgreen4"
+}
+
+ATTACK_DICT = {
+    cell_terrain.Terrain.Open: 2,
+    cell_terrain.Terrain.Forest: 0
+}
+
+DEFENSE_DICT = {
+    cell_terrain.Terrain.Open: 0,
+    cell_terrain.Terrain.Forest: 2
+}
+
+class Cell:
+    def __init__(self, terrain):
+        self.terrain = terrain
+
+    # TODO: replace this with a data member instead
+    # of a function.
+    def get_color(self):
+        return COLOR_DICT[self.terrain]
+    
+    def get_attack_mod(self):
+        return ATTACK_DICT[self.terrain]
+
+    def get_defense_mod(self):
+        return DEFENSE_DICT[self.terrain]
---
+++ game_map.py
@@ -0,0 +1,46 @@
+# GameMap class
+# Not much going on here. Builds the map cell by cell
+# with a random terrain type.
+
+import random
+import vec2
+import cell
+import params
+import cell_terrain
+
+CELL_TRANSLATION_MAP = {0: cell_terrain.Terrain.Open, 1: cell_terrain.Terrain.Forest}
+
+class GameMap:
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        self.cells = {}
+
+
+        self.gen_board()
+    
+    def gen_board(self):
+        noise = [[0 for _ in range(self.width)] for _ in range(self.height)]
+        board = [[0 for _ in range(self.width)] for _ in range(self.height)]
+        for j, row in enumerate(noise):
+            for i, tile in enumerate(row):
+                noise[j][i] = random.randint(0, 10)
+
+        for j, row in enumerate(noise):
+            for i, tile in enumerate(row):
+                tile_sum = 0
+
+                for direction in [[-1, 0], [1, 0], [0, 1], [0, -1], [-1, -1], [1, 1], [1, -1], [-1, 1], [0, 0]]:
+                    x, y = i + direction[0], j + direction[1]
+                    
+                    if x in range(len(noise[0])) and y in range(len(noise)):
+                        tile_sum += noise[y][x]
+                
+                board[j][i] = int(tile_sum > 50)
+        
+        for j, row in enumerate(board):
+            for i, tile in enumerate(row):
+                self.cells[vec2.Vec2(i, j)] = cell.Cell(CELL_TRANSLATION_MAP[tile])
+
+    def get_cell(self, pos):
+        return self.cells[pos]
